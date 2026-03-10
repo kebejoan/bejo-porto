bb1895691fdfc81cf62c80ad2c671980e6ffd923
kebejoan <jmailed.joe@gmail.com>
2026-03-10 22:42:25 +0700

    feat: fill in content education

--- app/components/1_AboutMe.tsx
+++ app/components/1_AboutMe.tsx
@@ -1,43 +1,46 @@
 export default function AboutMe() {
-	return (
-		<div className="w-full flex flex-col-reverse lg:flex-row gap-2">
-			<div className="w-full lg:w-3/4">
-				<div className="w-full flex flex-col items-center gap-4 outline-1">
-					<div className="w-full flex flex-col gap-4 p-4 outline-2">
-						<div className="text-3xl md:text-4xl lg:text-5xl font-pixel">
-							Hey there, My name is Faishal! 👋
-						</div>
-						<div className="w-full outline-1"></div>
-						<div className="">
-							People usually calls me <strong>Joe/Bejo</strong>. I am an{" "}
-							<strong>SCADA System Engineer and a Full-Stack Developer.</strong>
-							<br />
-							{/* dedicated to architecting reliable, high-performance systems
+  return (
+    <div className="w-full flex flex-col-reverse lg:flex-row gap-2">
+      <div className="w-full lg:w-3/4">
+        <div className="w-full flex flex-col items-center gap-4 outline-1">
+          <div className="w-full flex flex-col gap-4 p-4 outline-2">
+            <div className="text-3xl md:text-4xl lg:text-5xl font-pixel">
+              Hey there, My name is Faishal! 👋
+            </div>
+            <div className="w-full outline-1"></div>
+            <div className="">
+              People usually calls me <strong>Joe/Bejo</strong>. I am a{" "}
+              <strong>SCADA System Engineer and a Full-Stack Developer.</strong>
+              <br />
+              {/* dedicated to architecting reliable, high-performance systems
 							through data-driven and well-reasoned technical decisions.  */}
-							With over four years of experience delivering mission-critical
-							industrial solutions using{" "}
-							<strong>
-								STARDOM Controller, VDS HMI, SCADA CI Server, and 3rd Party HMI
-								Touch Panel
-							</strong>
-							, I bring a disciplined engineering rigor to modern web ecosystems
-							like <strong>TypeScript, Next.js, and Nest.js.</strong> Graduated
-							at the top of my Full-Stack Software Engineering bootcamp, I have
-							proven a rapid ability to build scalable software, including
-							developing custom <strong>Python ETL tools</strong> that reduced
-							operational workflows by <strong>90%.</strong>
-							<br /> I am excited to leverage this system-driven mindset to
-							develop applications that prioritize stability and execution
-							excellence.
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="w-full lg:w-1/4 flex flex-col justify-center items-center outline-2 p-4 lg:p-0">
-				<div className="w-48 lg:w-full max-w-sm">
-					<img src="/ava.png" alt="me" className="w-full h-auto rounded-xl" />
-				</div>
-			</div>
-		</div>
-	);
+              With over four years of experience delivering mission-critical
+              industrial solutions using{" "}
+              <strong>
+                STARDOM Controller, VDS HMI, SCADA CI Server, and 3rd Party HMI
+                Touch Panel
+              </strong>
+              , I bring a disciplined engineering rigor to modern web ecosystems
+              like <strong>TypeScript, Next.js, and Nest.js.</strong> Graduated
+              at the top of my Full-Stack Software Engineering bootcamp, I have
+              proven a rapid ability to build scalable software, including
+              developing custom <strong>Python ETL tools</strong> that reduced
+              operational workflows by <strong>90%.</strong>
+              <br /> I am excited to leverage this system-driven mindset to
+              develop applications that prioritize stability and execution
+              excellence.
+            </div>
+            <div className="w-full flex justify-end">
+              <div className="text-blue-700">...tell me more</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full lg:w-1/4 flex flex-col justify-center items-center outline-2 p-4 lg:p-0">
+        <div className="w-48 lg:w-full max-w-sm">
+          <img src="/ava.png" alt="me" className="w-full h-auto rounded-xl" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/components/2_Education.tsx
+++ app/components/2_Education.tsx
@@ -1,29 +1,58 @@
 export default function Education() {
-	return (
-		<div className="w-full grid grid-cols-4 gap-2">
-			<div className="w-full col-span-4">
-				<div className="w-full flex flex-col items-center gap-4 w-full outline-1 grid-span-2">
-					<div className="w-full flex flex-col gap-2 p-4 outline-2">
-						<div className="text-4xl font-bold">Education</div>
-						<div className="w-full outline-1"></div>
-						<div className="w-full grid grid-cols-6 gap-2">
-							<div className="w-full outline-2 row-span-3">Picture</div>
-							<div className="w-full outline-2 col-span-3">Faculty</div>
-							<div className="w-full outline-2 col-span-2">Time</div>
-							<div className="w-full outline-2 col-span-5">Major</div>
-							<div className="w-full outline-2 col-span-5">Content</div>
-						</div>
-						<div className="w-full grid grid-cols-6 gap-2">
-							<div className="w-full outline-2 row-span-3">Picture</div>
-							<div className="w-full outline-2 col-span-3">Faculty</div>
-							<div className="w-full outline-2 col-span-2">Time</div>
-							<div className="w-full outline-2 col-span-5">Major</div>
-							<div className="w-full outline-2 col-span-5">Content</div>
-						</div>
-						<div className="w-full flex justify-end">...more</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full grid grid-cols-4 gap-2">
+      <div className="w-full col-span-4">
+        <div className="w-full flex flex-col items-center gap-4 outline-1 grid-span-2">
+          <div className="w-full flex flex-col gap-4 p-4 outline-2">
+            <div className="text-3xl md:text-4xl lg:text-5xl font-pixel">
+              Education
+            </div>
+            <div className="w-full outline-1"></div>
+            <div className="w-full grid grid-cols-6 gap-2">
+              <div className="w-full outline-2 row-span-3 text-center content-center">
+                Picture
+              </div>
+              <div className="w-full outline-2 col-span-3 font-bold">RevoU</div>
+              <div className="w-full outline-2 col-span-2 text-end font-bold">
+                Feb 2025 - Aug 2025
+              </div>
+              <div className="w-full outline-2 col-span-5 italic">
+                Full-Stack Software Engineering
+              </div>
+              <div className="w-full outline-2 col-span-5">
+                Mastered full-stack development through an intensive program,
+                engineering end-to-end applications with TypeScript, Next.js,
+                and Nest.js while architecting type-safe databases using
+                PostgreSQL, Prisma ORM, and Supabase
+              </div>
+            </div>
+            <div className="w-full grid grid-cols-6 gap-2">
+              <div className="w-full outline-2 row-span-3 text-center content-center">
+                Picture
+              </div>
+              <div className="w-full outline-2 col-span-3 font-bold">
+                Telkom University
+              </div>
+              <div className="w-full outline-2 col-span-2 text-end font-bold">
+                Aug 2016 - March 2020
+              </div>
+              <div className="w-full outline-2 col-span-5 italic">
+                Bachelor’s of Physics Engineering
+              </div>
+              <div className="w-full outline-2 col-span-5">
+                <strong>GPA </strong>: 3.72/4.00 <br />
+                <strong>Final Project</strong>: “Alat Pemantau Kualitas Udara
+                Dalam Ruang Berbasis Internet of Things” <br />
+                <strong>Publication</strong>: “Analysis of Indoor Air Quality
+                Based on Low-Cost Sensor”
+                <br />
+                <strong>Patent</strong>: DJKI - S00202006865
+              </div>
+            </div>
+            {/* <div className="w-full flex justify-end">...more</div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
