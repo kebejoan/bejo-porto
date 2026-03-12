333d174b4970726be9bc7f73c2f8d4a3f0c478dc
kebejoan <jmailed.joe@gmail.com>
2026-03-12 10:16:44 +0700

    feat: add tagline in Groundhero, add more color scheme

--- app/components/GroundHero.tsx
+++ app/components/GroundHero.tsx
@@ -27,9 +27,12 @@ export default function GroundHero() {
 					className="w-auto h-[250px] md:h-[320px] lg:h-[400px] opacity-100 hidden dark:block z-0"
 				/>
 			</div>
-			<div className="absolute top-45 w-full flex flex-col justify-center items-center gap-4">
-				<div className="w-full lg:w-[1024]  flex flex-col justify-center items-center text-[4rem] md:text-[6rem] lg:text-[8rem] text-wrap font-pixel">
+			<div className="absolute top-55 w-full flex flex-col justify-center items-center">
+				<div className="w-full leading-none lg:w-[1024]  flex flex-col justify-center items-center text-[4rem] text-center text-wrap md:text-[6rem] lg:text-[8rem] text-wrap font-pixel">
 					<div>Faishal Tahsiin</div>
+				</div>
+				<div className="w-full leading-none lg:w-[1024]  flex flex-col justify-center items-center text-[1.4rem] text-center text-wrap md:text-4xl lg:text-5xl text-wrap font-pixel mb-6 text-bgblueflip">
+					<div>"Industrial Grade Modern Web Application"</div>
 				</div>
 				<div className="w-full lg:w-[1024]  flex flex-col justify-center items-center text-xl md:text-2xl lg:text-3xl font-bold text-wrap text-center">
 					<div>
