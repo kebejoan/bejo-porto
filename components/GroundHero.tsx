171b2f68dc858f8a52aaa6497aaede7ea91fad4f
kebejoan <jmailed.joe@gmail.com>
2026-03-14 00:53:58 +0700

    feat: add header, implement dark/light mode

--- components/GroundHero.tsx
+++ components/GroundHero.tsx
@@ -3,67 +3,67 @@ import { AllStack } from "@/data/techstack";
 import Badge from "./badges";
 
 export default function GroundHero() {
-	return (
-		<div className="relative w-full h-screen">
-			<div className="flex flex-col justify-end items-center w-full h-full">
-				<img
-					src="/Ground.png"
-					alt="RancaUpas"
-					className="w-full h-auto opacity-100 block dark:hidden z-0"
-				/>
-				<img
-					src="/Ground_Night.png"
-					alt="RancaUpas"
-					className="w-full h-auto opacity-100 hidden dark:block z-0"
-				/>
-			</div>
-			<div className="absolute bottom-0 w-full flex justify-center">
-				<img
-					src="/Hero.png"
-					alt="Bejoy"
-					className="w-auto h-[250px] md:h-[320px] lg:h-[400px] opacity-100 block dark:hidden z-0"
-				/>
-				<img
-					src="/Hero_Torch.png"
-					alt="Bejoy"
-					className="w-auto h-[250px] md:h-[320px] lg:h-[400px] opacity-100 hidden dark:block z-0"
-				/>
-			</div>
-			<div className="absolute top-55 w-full flex flex-col justify-center items-center">
-				<div className="w-full leading-none lg:w-[1024]  flex flex-col justify-center items-center text-[4rem] text-center text-wrap md:text-[6rem] lg:text-[8rem] text-wrap font-pixel">
-					<div>Faishal Tahsiin</div>
-				</div>
-				<div className="w-full leading-none lg:w-[1024]  flex flex-col justify-center items-center text-[1.4rem] text-center text-wrap md:text-4xl lg:text-5xl text-wrap font-pixel mb-6 text-bgblueflip">
-					<div>"Industrial Grade Modern Web Application"</div>
-				</div>
-				<div className="w-full lg:w-[1024]  flex flex-col justify-center items-center text-xl md:text-2xl lg:text-3xl font-bold text-wrap text-center gap-4">
-					<div>
-						SCADA System Engineer • Industrial Automation • Full-Stack Developer
-					</div>
-					<div className="left-20 w-full lg:w-[768] flex items-center justify-center translate-x--10 overflow-hidden px-2">
-						<div className="-ml-1">
-							<div className="w-full flex gap-2 overflow-hidden">
-								{AllStack.map((stack, index) => (
-									<Badge text={stack} key={index} />
-								))}
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="absolute bottom-20 w-full flex flex-col justify-center items-center gap-4  overflow-hidden">
-				<div className="w-full lg:w-[1024] flex flex-col justify-center items-center text-3xl md:text-4xl lg:text-5xl font-pixel">
-					<p className="flex lg:hidden bg-background px-2 rounded-2xl">
-						Swipe down, take the tour!
-					</p>
-					<p className="hidden lg:flex bg-background px-2 rounded-2xl">
-						Scroll up, take the tour!
-					</p>
-				</div>
-				<div className="font-bold flex flex-row text-lg md:text-xl lg:text-3xl text-red-500 bg-black">
+  return (
+    <div className="relative w-full h-screen">
+      <div className="flex flex-col justify-end items-center w-full h-full">
+        <img
+          src="/Ground.png"
+          alt="RancaUpas"
+          className="w-full h-auto opacity-100 block dark:hidden z-0"
+        />
+        <img
+          src="/Ground_Night.png"
+          alt="RancaUpas"
+          className="w-full h-auto opacity-100 hidden dark:block z-0"
+        />
+      </div>
+      <div className="absolute bottom-0 w-full flex justify-center">
+        <img
+          src="/Hero.png"
+          alt="Bejoy"
+          className="w-auto h-[250px] md:h-[320px] lg:h-[400px] opacity-100 block dark:hidden z-0"
+        />
+        <img
+          src="/Hero_Torch.png"
+          alt="Bejoy"
+          className="w-auto h-[250px] md:h-[320px] lg:h-[400px] opacity-100 hidden dark:block z-0"
+        />
+      </div>
+      <div className="absolute top-55 w-full flex flex-col justify-center items-center">
+        <div className="w-full leading-none lg:w-[1024]  flex flex-col justify-center items-center text-[4rem] text-center text-wrap md:text-[6rem] lg:text-[8rem] text-wrap font-pixel">
+          <div>Faishal Tahsiin</div>
+        </div>
+        <div className="w-full leading-none lg:w-[1024]  flex flex-col justify-center items-center text-[1.4rem] text-center text-wrap md:text-4xl lg:text-5xl text-wrap font-pixel mb-6 text-bgblueflip">
+          <div>"Industrial Grade Modern Web Application"</div>
+        </div>
+        <div className="w-full lg:w-[1024]  flex flex-col justify-center items-center text-xl md:text-2xl lg:text-3xl font-bold text-wrap text-center gap-4">
+          <div>
+            SCADA System Engineer • Industrial Automation • Full-Stack Developer
+          </div>
+          <div className="left-20 w-full lg:w-[768] flex items-center justify-center translate-x--10 overflow-hidden px-2">
+            <div className="-ml-1">
+              <div className="w-full flex gap-2 overflow-hidden">
+                {AllStack.map((stack, index) => (
+                  <Badge text={stack} key={index} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-20 w-full flex flex-col justify-center items-center gap-4  overflow-hidden">
+        <div className="w-full lg:w-[1024] flex flex-col justify-center items-center text-3xl md:text-4xl lg:text-5xl font-pixel">
+          <p className="flex xl:hidden bg-background px-2 rounded-2xl">
+            Swipe down, take a tour!
+          </p>
+          <p className="hidden xl:flex bg-background px-2 rounded-2xl">
+            Scroll up, take a tour!
+          </p>
+        </div>
+        {/* <div className="font-bold flex flex-row text-lg md:text-xl lg:text-3xl text-red-500 bg-black">
 					<div>{"🚧 UNDERCONSTRUCTION 🚧"}</div>
-				</div>
-			</div>
-		</div>
-	);
+				</div> */}
+      </div>
+    </div>
+  );
 }
--- components/HeaderPorto.tsx
+++ components/HeaderPorto.tsx
@@ -1,15 +1,41 @@
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
 export default function HeaderPorto() {
-	return (
-		<div className="w-full h-[72px] sticky bottom-0 p-2 flex justify-center content-center">
-			<div className="w-full h-full">
-				<div className="flex items-center justify-between w-full h-full text-3xl text-blue-200 font-bold opacity-80 px-4 py-2">
-					<div className="cursor-pointer">.kebejoan</div>
-					<div className="hidden sm:flex gap-4 ">
-						<div className="cursor-pointer">_</div>
-						<div className="cursor-pointer">|||</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted)
+    return (
+      /* Return a placeholder to prevent the layout from jumping */
+      <div className="w-full h-[72px] fixed bottom-0 left-0 p-4 opacity-0" />
+    );
+
+  return (
+    <div className="w-full h-[72px] fixed bottom-0 left-0 p-2 flex justify-center content-center">
+      <div className="w-full h-full">
+        <div className="hidden xl:flex items-center justify-between w-full h-full text-3xl text-white font-bold px-4 py-2">
+          <a href="https://kebejoan.vercel.app" className="cursor-pointer">
+            .kebejoan
+          </a>
+          <button
+            onClick={() =>
+              setTheme(resolvedTheme === "dark" ? "light" : "dark")
+            }
+            className="hidden sm:flex gap-4 "
+          >
+            <div className="cursor-pointer">
+              <FontAwesomeIcon
+                icon={resolvedTheme === "dark" ? faSun : faMoon}
+              />
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
