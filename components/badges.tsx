c5940ae2a53f33312644d4f22218917da8382f2b
kebejoan <jmailed.joe@gmail.com>
2026-03-16 15:26:02 +0700

    fix: stack can't have space

--- components/badges.tsx
+++ components/badges.tsx
@@ -1,6 +1,6 @@
 export default function Badge({ text }: { text: string }) {
 	return (
-		<p className="leading-none w-fit text-sm lg:text-base cursor-default default py-1 px-2 h-fit font-google font-bold bg-bgblueflip text-bgblue hover:bg-blue-500 rounded-full">
+		<p className="leading-none text-sm lg:text-base cursor-default default py-1 px-2 h-fit font-google font-bold bg-bgblueflip text-bgblue hover:bg-blue-500 rounded-full">
 			{text}
 		</p>
 	);
